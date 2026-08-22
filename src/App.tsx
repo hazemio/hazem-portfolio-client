d94ec6c93febb33422fce90fefc9172c8a731cd9
hazemio <hazemgmall04@gmail.com>
2026-08-22 21:52:47 +0300

    Update portfolio and admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ export default function App() {
             element={
               <Suspense fallback={<Spin />}>
                 <AdminDashboard />
-              </Suspense>
+              </Suspense >
             }
           />
           <Route
